f189c0d9035792526021dc33160b80cb3f4be32b
masazaki16007640 <masa.zaki@student.giu-uni.de>
2025-11-12 15:36:34 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -5,7 +5,22 @@
 
 //define enclosure class here 
 
-
+private:
+    Animal * m;
+    int capacity;
+    int currentCount;
+    
+    public:
+    Enclosure();
+    Enclosure(&m,int c, int curr);
+    void addAnimal(Animal * a);
+    void displayAnimals();
+    ~Enclosure();
+    void setCapacity();
+    void setCurrentCount();
+    int getCapacity();
+    int getCurrentCount();
+};
 
 
 
