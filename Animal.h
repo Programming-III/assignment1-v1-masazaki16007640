48a765630f79e5777ff7568b3f58e65e7cbeb606
masazaki16007640 <masa.zaki@student.giu-uni.de>
2025-11-12 15:06:44 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -5,7 +5,28 @@
 using namespace std;
 
 //define animal class here 
-
+class Animal{
+    private:
+    string name;
+    int age;
+    bool isHungry;
+    
+    public:
+    Animal();
+    Animal(string n, int a, bool h);
+    void display();
+    void feed();
+    void setName(string n);
+    void setAge(int a);
+    string getName();
+    int getAge();
+    ~Animal();
+    
+    
+    
+    
+    
+};
 
 
 #endif
