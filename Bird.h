8d801d900c53b3aac68e0de6a937bed76078c5a9
masazaki16007640 <masa.zaki@student.giu-uni.de>
2025-11-12 15:15:30 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -4,7 +4,19 @@
 #include "Animal.h"
 
 //define class bird here 
-
+class Bird:public Animal{
+    private:
+    double wingSpan;
+    
+    
+    public:
+    Bird();
+    Bird(string n, int a, bool h, double w);
+    double getWingSpan();
+    void setWingSpan(double w);
+    void display();
+    ~Bird();
+};
 
 
 
