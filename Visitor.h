d6c44216def0786f054031cba6e10462eeb1d561
masazaki16007640 <masa.zaki@student.giu-uni.de>
2025-11-12 15:36:00 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -5,7 +5,21 @@
 using namespace std;
 
 //define visitor class here
-
+class Visitor{
+    private:
+    string visitorName;
+    int ticketsBought;
+    
+    public:
+    Visitor();
+    Visitor(string n, int t);
+    ~Visitor();
+    void setVisitorName(string n);
+    void setTicketsBought(int t);
+    string getVisitorName();
+    int getTicketsBought();
+    void displayInfo();
+};
 
 
 
