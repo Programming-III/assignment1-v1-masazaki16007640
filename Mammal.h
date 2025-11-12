b620e255c1dfae4c9fa43f64131d2c16363ca2f0
masazaki16007640 <masa.zaki@student.giu-uni.de>
2025-11-12 15:16:06 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -5,7 +5,19 @@
 
 //define mammal class here
 
-
+class Mammal:public Animal{
+    private:
+    string furColor;
+    
+    
+    public:
+    Mammal();
+    Mammal(string n, int a, bool h, string f);
+    string getFurColor();
+    void setFurColor(string f);
+    void display();
+    ~Mammal();
+};
 
 
 
